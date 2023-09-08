2f185f2f930bc687b6c26f298c7837c62fabd24b
= <=>
2023-09-08 14:39:19 -0400

    settings bar- add toggle and style

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import SettingsBar from "./components/SettingsBar";
 import SearchBar from "./components/SearchBar";
 import Keyword from "./components/Keyword";
 import Definitions from "./components/Definitions";
-
 function App() {
   const [keyword, setKeyword] = useState("");
   const [meanings, setMeanings] = useState("");
   const [word, setWord] = useState("");
   const [phonetic, setPhonetic] = useState("");
+  const [sourceUrl, setSourceUrl] = useState("");
+  const [theme, setTheme] = useState("light");
 
   const onChange = (event) => {
     setKeyword(event.target.value);
@@ -22,6 +23,11 @@ function App() {
     }
   };
 
+  const toggleTheme = (e) => {
+    setTheme(theme === "light" ? "dark" : "light");
+    document.body.className = theme;
+  };
+
   const handleSubmit = () => {
     axios
       .get(`https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`)
@@ -29,6 +35,7 @@ function App() {
         const data = res.data[0];
         setWord(data.word);
         setPhonetic(data.phonetic);
+        setSourceUrl(data.sourceUrls[0]);
         setMeanings(data.meanings);
       })
       .catch((error) => {
@@ -43,6 +50,7 @@ function App() {
       </div>
       <div className="search">
         <SearchBar
+          toggleTheme={toggleTheme}
           handleKeypress={handleKeypress}
           handleSubmit={handleSubmit}
           onChange={onChange}
@@ -56,6 +64,7 @@ function App() {
           <Definitions meanings={meanings} />
         </div>
       )}
+      <p>{sourceUrl}</p>
     </div>
   );
 }
